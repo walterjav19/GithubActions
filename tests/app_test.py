ab67c698e491772793eed965e917178f365b5aac
Walter Santizo <walterjav19@gmail.com>
2024-07-11 18:44:45 -0600

    prueba 4

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,59 +1,59 @@
-import pytest
+import unittest
 from src.app import app
 
+class TestApp(unittest.TestCase):
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
 
+    def test_get_main(self):
+        response = self.client.get('/')
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), '<h1>Home</h1>')
 
+    def test_get_authors(self):
+        response = self.client.get('/author')
+        
+        self.assertEqual(response.status_code, 200)
+        authors_list = response.get_json()
+        self.assertEqual(len(authors_list), 11)
+        
+        self.assertEqual(authors_list[0]['id'], 1)
+        self.assertEqual(authors_list[0]['name'], "JK Rowling")
+        self.assertEqual(authors_list[0]['email'], "jkrwling@hotmail.com")
+        self.assertEqual(authors_list[0]['age'], 56)
+    
+    def test_get_genres(self):
+        response = self.client.get('/genre')
+        
+        self.assertEqual(response.status_code, 200)
+        genres_list = response.get_json()
+        self.assertEqual(len(genres_list), 7)
+        
+        self.assertEqual(genres_list[2]['id'], 3)
+        self.assertEqual(genres_list[2]['name'], "Mystery")
+        
+        self.assertEqual(genres_list[1]['id'], 2)
+        self.assertEqual(genres_list[1]['name'], "Horror")
 
-def test_get_main(client):
-    response = client.get('/')
-    
-    assert response.status_code == 200
-    assert response.data.decode('utf-8') == '<h1>Home</h1>'
+    def test_get_editorials(self):
+        response = self.client.get('/editorial')
+        
+        self.assertEqual(response.status_code, 200)
+        editorials_list = response.get_json()
+        self.assertEqual(len(editorials_list), 7)
+        
+        self.assertEqual(editorials_list[0]['id'], 1)
+        self.assertEqual(editorials_list[0]['name'], "dibu")
+        self.assertEqual(editorials_list[0]['location'], "GUATEMALA")
+        self.assertEqual(editorials_list[0]['phone'], "123231")
+        
+        self.assertEqual(editorials_list[1]['id'], 2)
+        self.assertEqual(editorials_list[1]['name'], "HarperCollins")
+        self.assertEqual(editorials_list[1]['location'], "New York")
+        self.assertEqual(editorials_list[1]['phone'], "212-555-5678")
 
-
-def test_get_authors(client):
-    response = client.get('/author')
-    
-    assert response.status_code == 200
-    authors_list = response.get_json()
-    assert len(authors_list) == 11
-    
-    assert authors_list[0]['id'] == 1
-    assert authors_list[0]['name'] == "JK Rowling"
-    assert authors_list[0]['email'] == "jkrwling@hotmail.com"
-    assert authors_list[0]['age'] == 56
-    
-def test_get_genres(client):
-    response = client.get('/genre')
-    
-    assert response.status_code == 200
-    genres_list = response.get_json()
-    assert len(genres_list) == 7
-    
-    assert genres_list[2]['id'] == 3
-    assert genres_list[2]['name'] == "Mystery"
-    
-    assert genres_list[1]['id'] == 2
-    assert genres_list[1]['name'] == "Horror"
-
-def test_get_editorials(client):
-    response = client.get('/editorial')
-    
-    assert response.status_code == 200
-    editorials_list = response.get_json()
-    assert len(editorials_list) == 7
-    
-    assert editorials_list[0]['id'] == 1
-    assert editorials_list[0]['name'] == "dibu"
-    assert editorials_list[0]['location'] == "GUATEMALA"
-    assert editorials_list[0]['phone'] == "123231"
-    
-    assert editorials_list[1]['id'] == 2
-    assert editorials_list[1]['name'] == "HarperCollins"
-    assert editorials_list[1]['location'] == "New York"
-    assert editorials_list[1]['phone'] == "212-555-5678"
+if __name__ == '__main__':
+    unittest.main()
